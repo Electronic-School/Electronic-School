67dda55084193f93f083d18b1e34b64360da32b3
Abdullah-saleh-s <mahyoub2110@gmail.com>
2025-11-21 23:18:10 +0300

    Adding the windows forms for the user login and admin forms

---
+++ ElectronicSchoolSystem/userLogIn.cs
@@ -0,0 +1,15 @@
+namespace ElectronicSchoolSystem
+{
+    public partial class userLogIn : Form
+    {
+        public userLogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
