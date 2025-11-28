bdbff524b0d936b7509976da3c97e6b35fa7423b
Abdullah-saleh-s <mahyoub2110@gmail.com>
2025-11-28 05:13:33 +0300

    Creating all the Models (each model meet a table at the database)

---
+++ SchoolManagementSystem/Models/Location.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Models
+{
+    public class Location
+    {
+        [Key]
+        public int LocationId { get; set; }
+
+        [Required]
+        public int CountryId { get; set; }
+        [ForeignKey("CountryId")]
+        public required Country Country { get; set; }
+
+        [Required]
+        public int CityId { get; set; }
+
+        [ForeignKey("CityId")]
+        public required City City { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column(TypeName = "varchar(50)")] 
+        public required string Street { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Column(TypeName = "varchar(30)")] 
+        public required string BuildingNo { get; set; }
+
+        public ICollection<Parent> Parents { get; set; } = new List<Parent>();
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+        public ICollection<Teacher> Teachers { get; set; } = new List<Teacher>();
+        public ICollection<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
---
+++ SchoolManagementSystem/Models/Teacher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public int TeacherId { get; set; }
+
+        [StringLength(50)]
+        public required string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public required string LastName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        [Required]
+        public int LocationId { get; set; }
+
+        [ForeignKey("LocationId")]
+        public required Location Location { get; set; }
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal? Salary { get; set; }
+
+        [StringLength(100)]
+        public required string EducationDegree { get; set; }
+
+        [StringLength(100)]
+        public required string TeachingSubject { get; set; }
+
+        public DateTime? StartWorkingDate { get; set; }
+
+        public int? NumberOfVacations { get; set; }
+
+        [StringLength(15)]
+        public required string PhoneNumber { get; set; }
+
+        [StringLength(100)]
+        public required string Email { get; set; }
+
+        [StringLength(50)]
+        public required string SocialStatus { get; set; }
+
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
