e0a892e85ad626a7428077c4f94e7d66de10850d
Abdullah-saleh-s <mahyoub2110@gmail.com>
2025-12-04 23:39:54 +0300

    Creating the first version of main form and adding the teacher form

---
+++ SchoolSystem/MDIMainFormAdministrationForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SchoolSystem.Controls;
+
+namespace SchoolSystem
+{
+    public partial class MDIMainFormAdministrationForm : Form
+    {
+        //private int childFormNumber = 0;
+        private Button currentButton;
+
+
+        public MDIMainFormAdministrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.5f, System.Drawing.FontStyle.Regular);
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelSidebar.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 43);
+                    previousBtn.ForeColor = Color.Gainsboro;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.5f, System.Drawing.FontStyle.Regular);
+                }
+            }
+        }
+
+        private void LoadUserControl(UserControl us)
+        {
+            panelContent.Controls.Clear();
+
+            us.Dock = DockStyle.Fill;
+
+            panelContent.Controls.Add(us);
+
+        }
+
+
+
+        private void btnSidebarDashboard_Click(object sender, EventArgs e)
+        {
+            TeacherManagementUC teacherUC = new TeacherManagementUC();
+            LoadUserControl(teacherUC);
+        }
+
+        private void btnSidebarStudentManage_Click(object sender, EventArgs e)
+        {
+            panelContent.Controls.Clear();
+            LoadUserControl(new AddStudentControl());
+        }
+
+        private void btnSidebarTeacherManage_Click(object sender, EventArgs e)
+        {
+            TeacherManagementUC teacherUC = new TeacherManagementUC();
+            LoadUserControl(teacherUC);
+        }
+    }
+}
